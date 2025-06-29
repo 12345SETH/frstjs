71b561908c7bc2110b724b10638ca7e6125d688b
Dhruv Seth <166598352+12345SETH@users.noreply.github.com>
2025-06-29 16:25:48 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+// console.log(null>0)  false
+// console.log(null>=0)  comparison null is converted as zero - true
+// console.log(null==0)  here null is not converted to zero  - false
+
+// "===" = COMPARISON OF DATA TYPES 
+console.log(2===2)
+console.log("2"===2)
