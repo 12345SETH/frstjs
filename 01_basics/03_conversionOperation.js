20c71b31a5ecfa12088b2d80a633062d8fe14930
Dhruv Seth <166598352+12345SETH@users.noreply.github.com>
2025-06-29 15:29:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+// let score = "18dns"
+// console.log(typeof score)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) // NOT A NUMBER
+/*
+"18" = 18
+"18dns" = NaN
+"null" = 0
+"undefined" = NaN
+"true/false" = 1/0
+*/
+let isLoggedIn = 0 // 1 - TRUE ; " " - FALSE ; " DNS " - TRUE
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn )
