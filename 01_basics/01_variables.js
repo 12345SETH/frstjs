e21981fe4b85d0df003bb0596f30991565f145e8
Dhruv Seth <166598352+12345SETH@users.noreply.github.com>
2025-06-29 14:33:36 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 133452
+let accountEmail = "dhruv@gmail.com"
+var accountPassword = " 12234"
+accountCity = "Jaipur"
+let accountState
+// accountId = 2
+/*
+NOTE - WE NEVER USE VAR AS A VARIBLE BECAUSE IT CAUSES ERROR IN BLOCK SCOPE AND FUNCTIONAL SCOPE
+*/
+accountEmail ="ds@gamil.com"
+accountPassword ="1442"
+accountCity = "Bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+  "use strict"; // NEWER VERSION OF JS
+//  VARIOUS  DATA TYPES  - 
+let nameS= "DNS" // STRING - " "
+let age = 19 // NUMBER - RANGE = 2^53 
+let isLoggedIn = true // BOOLEAN VALUE - TRUE(1) OR FALSE(0)
+// NULL - STANDALONE VALUE
+// UNDEFINED 
+// SYMBOL - UNIQUE
+// OBJECT 
+console.log(typeof null) // OBJECT
+console.log(typeof undefined) // UNDEFINED
