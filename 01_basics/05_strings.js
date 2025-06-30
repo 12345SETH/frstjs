a9485053d9a79d4e1e890ae7b36c1b9650df1622
Dhruv Seth <166598352+12345SETH@users.noreply.github.com>
2025-06-30 20:43:30 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const name =  "DNS"
+const repoCount = 1
+// console.log(`hi my name is ${name} and my repo count is ${repoCount}`)
+const gameName = new String(`Dhruv-NS`)
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(5))
+// console.log(gameName.indexOf('N'))
+// const newString = gameName.substring(0,5)
+// console.log(newString)
+// const anotherString = gameName.slice(-6,5)
+// console.log(anotherString)
+const newStringOne = "   DNS   "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+const url = "https://Dns.com/Dns%20Dhruv"
+console.log(url.replace('%20','-'))
+console.log(url.includes('vu'))
+console.log(newStringOne.split('-'))
+
