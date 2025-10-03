922c97ff6f2493c7de5746bf1a1ef5b27f2a7df6
Dhruv Seth <166598352+12345SETH@users.noreply.github.com>
2025-10-03 06:25:26 +0000

    Math And Num

---
+++ 01_basics/06_NumberAndMaths.js
@@ -0,0 +1,26 @@
+const score  = 400;
+// console.log(score);
+
+const num =  new Number(420)
+// console.log(num);
+// console.log(num.toString().length);
+// console.log(num.toFixed(2));
+const anum =123.546
+// console.log(anum.toPrecision(3));// return string
+const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'));
+console.log(Math);
+console.log(Math.abs(-6));
+console.log(Math.round(4.8));
+console.log(Math.ceil(6.5));
+console.log(Math.floor(4.9));
+console.log(Math.min(6,4,8,3));
+console.log(Math.max(6,4,8,3));
+
+// console.log(Math.random());  give value range from 0 to 1 only
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+  
