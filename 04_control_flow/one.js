01262755ed284f60b099a9a19e07d4bda780d053
Dhruv Seth <166598352+12345SETH@users.noreply.github.com>
2025-10-07 13:30:43 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,9 @@
+// const isUserLoggedIn = true
+// if(isUserLoggedIn){
+// console.log("Executed");
+// }
+const value = 200
+if(value>100){
+    let power = " fly"
+    console.log(`Power Value : ${power}`);
+}
